96507276c6fb4e6dc8107ffabef242603e50d2b9
AGonzL <agonzlopez.11@gmail.com>
2023-11-24 00:38:07 +0100

    Project Recovery and First Commit

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "MovingPlatform.h"
+#include "MovingPlatform.h"//incluimos el header de la clase
 
 // Sets default values
-AMovingPlatform::AMovingPlatform()
+AMovingPlatform::AMovingPlatform()//llamamos al constructor de la clase padre
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,16 +12,48 @@ AMovingPlatform::AMovingPlatform()
 }
 
 // Called when the game starts or when spawned
-void AMovingPlatform::BeginPlay()
+void AMovingPlatform::BeginPlay()//llamamos a la funcion beginplay cuando empieza el juego
 {
-	Super::BeginPlay();
-	
-}
+	Super::BeginPlay();//llamamos a la funcion beginplay de la clase padre
 
+	StartLocation = GetActorLocation();//guardamos la localizacion inicial de la plataforma
+}
 // Called every frame
-void AMovingPlatform::Tick(float DeltaTime)
+void AMovingPlatform::Tick(float DeltaTime)//llamamos a la funcion tick cada frame
 {
-	Super::Tick(DeltaTime);
+	Super::Tick(DeltaTime);//llamamos a la funcion tick cada frame
+
+	MovePlatform(DeltaTime);//movemos la plataforma
+	RotatePlatform(DeltaTime);//rotamos la plataforma	
+}
+void AMovingPlatform::MovePlatform(float DeltaTime)//movemos la plataforma
+{
+
+	if (ShouldPlatformReturn())  //si la plataforma se ha movido mas de la distancia que le hemos indicado, la hacemos volver
+	{	
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();    //normalizamos el vector de velocidad para que solo nos de la direccion en la que se mueve
+		StartLocation = StartLocation + (MoveDirection * PlatformDistance); //actualizamos la localizacion de la plataforma
+		SetActorLocation(StartLocation);//actualizamos la localizacion del actor
+		PlatformVelocity = -PlatformVelocity; //cambiamos la direccion de la velocidad para que vuelva
+	}
+	else
+	{
+		FVector CurrentLocation = GetActorLocation();       //aqui actualizamos la localizacion del actor
+	    CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);//aqui actualizamos la localizacion del actor
+		SetActorLocation(CurrentLocation);//aqui actualizamos la localizacion del actor
+
+	}
 
 }
-
+void AMovingPlatform::RotatePlatform(float DeltaTime)//rotamos la plataforma
+{
+	AddActorLocalRotation(RotationVelocity * DeltaTime);//rotamos la plataforma
+}
+bool AMovingPlatform::ShouldPlatformReturn() const//comprobamos si la plataforma se ha movido mas de la distancia que le hemos indicado
+{	
+	return GetDistanceMoved() > PlatformDistance;//comprobamos si la plataforma se ha movido mas de la distancia que le hemos indicado
+}
+float AMovingPlatform::GetDistanceMoved() const//calculamos la distancia que se ha movido la plataforma
+{
+	return FVector::Dist(StartLocation, GetActorLocation());//calculamos la distancia que se ha movido la plataforma
+}
--- Source/ObstacleAssault/MovingPlatform.h
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -2,18 +2,18 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "MovingPlatform.generated.h"
+#include "CoreMinimal.h"//incluimos el core de unreal
+#include "GameFramework/Actor.h"//incluimos la clase AActor
+#include "MovingPlatform.generated.h"//generamos el cuerpo de la clase
 
 UCLASS()
-class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor//heredamos de la clase AActor
 {
-	GENERATED_BODY()
+	GENERATED_BODY()//generamos el cuerpo de la clase
 	
 public:	
 	// Sets default values for this actor's properties
-	AMovingPlatform();
+	AMovingPlatform();//llamamos al constructor de la clase padre
 
 protected:
 	// Called when the game starts or when spawned
@@ -21,6 +21,22 @@ protected:
 
 public:	
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	virtual void Tick(float DeltaTime) override;//llamamos a la funcion tick cada frame
 
+private:	
+
+	UPROPERTY(EditAnywhere, Category="Moving") //velocidad de la plataforma
+	FVector PlatformVelocity = FVector(100, 0, 0);//velocidad de la plataforma
+	UPROPERTY(EditAnywhere, Category="Moving")//distancia que se mueve la plataforma 
+	float PlatformDistance = 100;//distancia que se mueve la plataforma
+	UPROPERTY(EditAnywhere, Category="Rotation")//velocidad de rotacion de la plataforma
+	FRotator RotationVelocity;//velocidad de rotacion de la plataforma
+	
+	FVector StartLocation;//localizacion inicial de la plataforma
+
+	void MovePlatform(float DeltaTime);//movemos la plataforma							
+	void RotatePlatform(float DeltaTime);//rotamos la plataforma
+
+	bool ShouldPlatformReturn() const;//comprobamos si la plataforma se ha movido mas de la distancia que le hemos indicado
+	float GetDistanceMoved() const;//calculamos la distancia que se ha movido la plataforma
 };
